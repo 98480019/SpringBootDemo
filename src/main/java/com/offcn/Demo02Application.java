4a7847f9d1058df1a4275e0a43662eefc707c239
98480019 <50939237+98480019@users.noreply.github.com>
2020-01-08 19:58:40 +0800

    修改主方法注释

--- src/main/java/com/offcn/Demo02Application.java
+++ src/main/java/com/offcn/Demo02Application.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo02Application {
     /**
-    *加个注释
-    */
+     * 主方法
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(Demo02Application.class, args);
     }
