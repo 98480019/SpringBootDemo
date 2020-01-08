90295ccf86beb79be53edbdae801823243563ad0
98480019 <50939237+98480019@users.noreply.github.com>
2020-01-08 20:41:35 +0800

    删除Demo02Application.java main方法的注释

--- src/main/java/com/offcn/Demo02Application.java
+++ src/main/java/com/offcn/Demo02Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo02Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Demo02Application.class, args);
     }
 }
