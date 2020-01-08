9c6670426ce52f721a9dadbc158e486baf8feaa1
98480019 <50939237+98480019@users.noreply.github.com>
2020-01-08 19:46:54 +0800

    Update Demo02Application.java
    在main方法加注释

--- src/main/java/com/offcn/Demo02Application.java
+++ src/main/java/com/offcn/Demo02Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo02Application {
-
+    /**
+    *加个注释
+    */
     public static void main(String[] args) {
         SpringApplication.run(Demo02Application.class, args);
     }
